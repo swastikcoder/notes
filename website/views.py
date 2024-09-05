aa632e1a310b6d01ea546e3f431c40ffbe7da738
Swastik Ranjan <swastik.ranjan@pal.tech>
2024-09-05 14:37:50 +0530

    Commit with updated code

---
+++ website/views.py
@@ -0,0 +1,33 @@
+from flask import Blueprint,render_template,request,flash,redirect,url_for
+from flask_login import  login_required, current_user
+from .models import Note
+from .import db
+views = Blueprint('views', __name__)
+
+@views.route('/', methods = ['GET', 'POST'])
+@login_required
+def home():
+        if request.method == 'POST':
+            note = request.form.get('note')
+            if len(note) < 1:
+                flash('Add something in the note ', category= 'error')
+            else:
+                new_note = Note(data = note, user_id = current_user.id)
+                db.session.add(new_note)
+                db.session.commit()
+                flash('Note added succesfully!', category = 'success')
+        return render_template("home.html",user = current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    noteId = request.form.get('noteId')
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+            flash('Note deleted successfully!', 'success')
+
+
+    return redirect(url_for('views.home'))
